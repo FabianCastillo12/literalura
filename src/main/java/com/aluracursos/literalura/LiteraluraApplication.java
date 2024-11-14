5fe2901716e7a0f5f1a1bfbc190dc7ae0e34dd89
FabianCastillo12 <132018077+FabianCastillo12@users.noreply.github.com>
2024-11-14 16:53:43 +0000

    "consola"

--- src/main/java/com/aluracursos/literalura/LiteraluraApplication.java
+++ src/main/java/com/aluracursos/literalura/LiteraluraApplication.java
@@ -1,13 +1,22 @@
 package com.aluracursos.literalura;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.aluracursos.literalura.Principal.AppPrincipal;
+
 @SpringBootApplication
-public class LiteraluraApplication {
+public class LiteraluraApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiteraluraApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		AppPrincipal appPrincipal = new AppPrincipal();
+		appPrincipal.mostrarMenu();
 	}
 
 }
---
+++ src/main/java/com/aluracursos/literalura/Principal/AppPrincipal.java
@@ -0,0 +1,7 @@
+package com.aluracursos.literalura.Principal;
+
+public class AppPrincipal {
+    public void mostrarMenu(){
+        System.out.println("Bienvenido a la Biblioteca de Literatura");
+    }
+}
